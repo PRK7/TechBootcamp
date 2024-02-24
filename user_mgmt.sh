d63dd0482ffe1218b2c81bca80ad57715d2120ea
PRK7 <prateek.r.karkera@gmail.com>
2024-02-24 20:57:46 +0530

    Added additional Functions

--- user_mgmt.sh
+++ user_mgmt.sh
@@ -41,13 +41,43 @@ removeUser() {
 	echo "User $username deleted successfully!"
 }
 
+listUsers() {
+	echo "List of all users:"
+	cut -d: -f1 /etc/passwd
+}
+
+modifyUserPermissions() {
+	read -p "Enter username to modify permissions: " username
+	read -p "Enter permission mode (e.g., 755, 700, 777): " permission_mode
+	sudo chmod $permission_mode /home/$username
+	echo "Permissions modified successfully for user $username."
+}
+
 addGroup() {
 	read -p "Enter group name to add: " groupname
 	sudo groupadd $groupname
 	echo "Group $groupname added successfully!"
 }
 
-usertoGroup() {
+removeGroup() {
+	read -p "Enter group name to remove: " groupname
+	sudo groupdel $groupname
+	echo "Group $groupname deleted successfully!"
+}
+
+listGroups() {
+	echo "List of all groups:"
+	cut -d: -f1 /etc/group
+}
+
+modifyGroupPermissions() {
+	read -p "Enter group name to modify permissions: " groupname
+	read -p "Enter permission mode (e.g., 755, 700, 777): " permission_mode
+	sudo chmod $permission_mode /home/$groupname
+	echo "Permissions modified successfully for group $groupname."
+}
+
+addUsertoGroup() {
 	read -p "Enter the username: " username
 	userexists=$(grep -w $username /etc/passwd)
 	if [ -z "$userexists" ]
@@ -62,6 +92,24 @@ usertoGroup() {
 	fi
 }
 
+removeUserfromGroup() {
+	read -p "Enter username: " username
+	read -p "Enter group name: " groupname
+	sudo deluser $username $groupname
+	echo "User $username removed from group $groupname successfully."
+}
+
+checkUserGroups() {
+	read -p "Enter username: " username
+	echo "Groups for user $username:"
+	groups $username | cut -d: -f2
+}
+
+checkGroupUsers() {
+	read -p "Enter group name: " groupname
+	echo "Users in group $groupname:"
+	getent group $groupname | cut -d: -f4
+}
 
 main() {
 	clear
@@ -75,17 +123,33 @@ main() {
 		echo "Please select an option: "
 		echo "1. Add user"
 		echo "2. Remove user"
-		echo "3. Add a group"
-		echo "4. Add user to a group"
-		echo "5. Exit"
+		echo "3. List all users"
+		echo "4. Modify user permissions"
+		echo "5. Add a group"
+		echo "6. Remove a group"
+		echo "7. List all groups"
+		echo "8. Modify group permissions"
+		echo "9. Add user to a group"
+		echo "10. Remove user from group"
+		echo "11. Check which groups a user is in"
+		echo "12. Check which users are in a particular group"
+		echo "13. Exit"
 		read -p "Enter your choice: " choice
 
 		case $choice in
 			1) addUser;;
 			2) removeUser;;
-			3) addGroup;;
-			4) usertoGroup;;
-			5) echo "Exiting..";
+			3) listUsers;;
+			4) modifyUserPermissions;;
+			5) addGroup;;
+			6) removeGroup;;
+			7) listGroups;;
+			8) modifyGroupPermissions;;
+			9) addUsertoGroup;;
+			10) removeUserfromGroup;;
+			11) checkUserGroups;;
+			12) checkGroupUsers;;
+			13) echo "Exiting..";
 				exit;;
 			*) echo "Invalid choice. Please try again.";;
 		esac
